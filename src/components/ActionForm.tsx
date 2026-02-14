a9839fcba135421cbde2800d4791aa8965047953
Adam AKA Adamjava <adamtroyamail@gmail.com>
2026-02-14 23:52:30 +0200

    second version

--- src/components/ActionForm.tsx
+++ src/components/ActionForm.tsx
@@ -1,24 +1,28 @@
 "use client";
 
 import { useTransition } from "react";
+import { useRouter } from "next/navigation";
 
 type ActionFormProps = Omit<React.ComponentPropsWithoutRef<"form">, "action"> & {
   action: (formData: FormData) => Promise<void> | void;
-  refreshMode?: "reload" | "none";
+  refreshMode?: "refresh" | "reload" | "none";
 };
 
 export default function ActionForm({
   action,
   children,
-  refreshMode = "reload",
+  refreshMode = "refresh",
   ...props
 }: ActionFormProps) {
   const [, startTransition] = useTransition();
+  const router = useRouter();
 
   const handleAction = async (formData: FormData) => {
     await action(formData);
     if (refreshMode === "reload") {
       window.location.reload();
+    } else if (refreshMode === "refresh") {
+      router.refresh();
     }
   };
 
